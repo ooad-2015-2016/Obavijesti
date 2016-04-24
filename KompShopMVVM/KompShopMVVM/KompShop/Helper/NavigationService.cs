98175b2ddf05724243f71d452f591f6873c55950
CogoEmir <cogo.emir@gmail.com>
2016-04-24 22:13:08 +0200

    Commentari na Navigation Service

--- KompShopMVVM/KompShopMVVM/KompShop/Helper/NavigationService.cs
+++ KompShopMVVM/KompShopMVVM/KompShop/Helper/NavigationService.cs
@@ -10,18 +10,19 @@ namespace KompShopMVVM.KompShop.Helper
 {
     class NavigationService:INavigationService
     {
+        //obicna navigacija bez parametra
         public void Navigate(Type sourcePage)
         {
             var frame = (Frame) Window.Current.Content;
             frame.Navigate(sourcePage);
         }
-
+        //navigiranje na page ali da se proslijedi parametar stranici
         public void Navigate(Type sourcePage, object parameter)
         {
             var frame = (Frame) Window.Current.Content;
             frame.Navigate(sourcePage, parameter);
         }
-
+        //poziv da se vrati na predhodnu stranicu
         public void GoBack()
         {
             var frame = (Frame) Window.Current.Content;
